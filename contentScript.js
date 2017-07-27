c7ceeefe673a1c413d3d39f22fd5855a5d1ae556
Ethan Manns <ethan.manns@connexta.com>
2017-07-27 14:48:37 -0700

    Removed alert

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,3 @@
-alert("contentScript.js");
 [].forEach.call(document.getElementsByClassName("ghx-backlog-container"), node => {
     node.classList.remove("ghx-open");
     node.classList.add("ghx-closed");
